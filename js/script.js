e57f815546d52168758fc80cc56e7e048f05dcb4
João Luciano <lucianoangeys@gmail.com>
2024-09-05 10:27:00 -0300

    Correção [O require() não funciona no navegador]

--- js/script.js
+++ js/script.js
@@ -1,35 +1,10 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const net = require('net');
-const atob = require('atob'); // para decodificar base64
-
-const app = express();
-app.use(bodyParser.json());
-
-app.post('/sendToTcp', (req, res) => {
-    const serverInfo = req.body.server.split(':');
-    const serverIp = serverInfo[0];
-    const serverPort = parseInt(serverInfo[1], 10);
-    const dataToSend = Buffer.from(atob(req.body.info), 'binary'); // Decodifica base64 para binário
-
-    const tcpClient = new net.Socket();
-
-    tcpClient.connect(serverPort, serverIp, () => {
-        tcpClient.write(dataToSend);
-    });
-
-    tcpClient.on('data', (data) => {
-        console.log('Resposta do servidor TCP:', data.toString());
-        res.json({ status: 'Data sent to TCP', response: data.toString() });
-        tcpClient.destroy();
-    });
-
-    tcpClient.on('error', (err) => {
-        console.error('Erro ao conectar ao TCP:', err.message);
-        res.status(500).json({ status: 'Error', message: err.message });
-    });
-});
-
-app.listen(3000, () => {
-    console.log('Servidor HTTP escutando na porta 3000');
-});
+fetch("https://vercel.com/joaoluciano1429s-projects/tcpip-to-https-github-io", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
+    server: "127.0.0.1:12345", // IP e porta do servidor TCP
+    info: btoa("mensagem a ser enviada"), // Codifica em base64
+  }),
+})
+  .then((response) => response.json())
+  .then((data) => console.log("Resposta:", data));
